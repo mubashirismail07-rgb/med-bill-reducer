6d354602b1a0f5b9b9490217320d39d32d2c5a0f
mubashirismail07-rgb <mubashirismail07@gmail.com>
2026-07-12 18:53:48 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://med-bill-reducer.vercel.app",
+    "https://med-bill-reducer.vercel.app/api",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
